7ce1b6d83a1e644eabf694317e517174a3dcd1cf
fedeeegarciia13 <federico.garcia.3a@gmail.com>
2026-08-20 11:40:23 -0300

    revertir main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import AppDesktop from "./AppDesktop.jsx";
 import "./index.css";
-
-const isDesktop = window.innerWidth >= 1024;
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {isDesktop ? <AppDesktop /> : <App />}
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>);
